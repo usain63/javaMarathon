d4cad4a01ce8dc93573d5c7235ce4a004e5a3da2
Eldar Mamedzade <eldarsheen@mail.ru>
2022-11-30 08:09:15 +0300

    9th day to review

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,14 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Anton", "Первая группа");
+        Teacher teacher = new Teacher("Михалыч", "Физкультура");
+
+        System.out.println("Группа студента: " + student.getGroupName());
+        System.out.println("Предмет учителя: " + teacher.getSubjectName());
+        
+        student.printInfo();
+        teacher.printInfo();
 
     }
 }
