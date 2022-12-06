eca16e21a7ae98a36fce7926a7ca28909b2e7fc8
Eldar Mamedzade <eldarsheen@mail.ru>
2022-12-06 11:11:33 +0300

    16th day for review

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,37 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("day14.txt");
+        printResult(file);
+
+    }
+    
+    public static void printResult(File file){
+        
+        try {
+            Scanner scanner = new Scanner(file);
+            double sum = 0;
+            double average = 0;
+            
+            String line = scanner.nextLine();
+            String[] arrayFromLine = line.split(" ");
+            for (String number: arrayFromLine) {
+                sum += Double.parseDouble(number);
+            }
+            
+            average = sum / arrayFromLine.length;
+            System.out.print(average + " --> " + String.format("%.3f", average));
+            
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
 
     }
 }
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,85 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
+        
+        saveToFile1(file1);
+        saveToFile2(file1, file2);
+        printResult(file2);
+    }
 
+    public static void saveToFile1(File file) {
+        Random random = new Random();
+        
+        try {
+            PrintWriter pw = new PrintWriter(file);
+            for (int i = 0; i < 1000; i++)
+                pw.print(random.nextInt(101) + " ");
+            pw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not found");
+        }
+    }
+    
+    public static void saveToFile2 (File file1, File file2) {
+        
+        int segmentSize = 20;
+        int sumOfASegment = 0;
+        double avgOfASegment = 0;
+       
+        try {
+            
+            Scanner scanner = new Scanner(file1);
+            PrintWriter pw = new PrintWriter(file2);
+            
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] arrFromLine = line.split(" ");
+                String[] subArrFromLine = new String[segmentSize];
+                
+                
+                for (int i = 0; i < arrFromLine.length - 19; i += segmentSize){
+                    subArrFromLine = Arrays.copyOfRange(arrFromLine, i, i + segmentSize);
+                    
+                    for (int j = 0; j < subArrFromLine.length; j++){
+                        sumOfASegment += Integer.parseInt(subArrFromLine[j]);
+                    }
+                    pw.print(((double) sumOfASegment / segmentSize) + " ");
+                    sumOfASegment = 0;
+                }
+            }
+            scanner.close();
+            pw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not found");
+        }
+    }
+    
+    public static void printResult(File file) {
+        int finalSum = 0;
+        
+        try {
+            Scanner scanner = new Scanner(file);
+            
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] arrFromLine = line.split(" ");
+
+                for (String num:arrFromLine) {
+                    finalSum += Double.parseDouble(num);
+                }
+                System.out.println(finalSum);
+            }
+                
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not found");
+        }
     }
 }
